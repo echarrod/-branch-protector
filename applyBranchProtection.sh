4edac92cbe651824f758bfcdfc901ac4f58b7352
Ed <echarrod@users.noreply.github.com>
2024-06-28 16:19:34 +0100

    Update after API errors

--- applyBranchProtection.sh
+++ applyBranchProtection.sh
@@ -11,49 +11,49 @@
 ##     $0 'main';  #Updates all main branch rules in the org
 ##     $0 'main' 'lib-' ;#Updates all main branch rules on the repos matching 'lib-'
 
-OWNER=Your_Org_Name   # Set your org name here
+OWNER=luno   # Set your org name here
 LIMIT=250                # Set this to some number higher than the number of repos you have
-BRANCH_PATTERN_LIST=${1:-"dev,stg,main,master"}; shift  # Either pass in a comma-delimited list of branches to protect or use the defaults
+BRANCH_PATTERN_LIST=${1:-"main,master"}; shift  # Either pass in a comma-delimited list of branches to protect or use the defaults
 FILTER=${1:""}; shift    # Pass a filter to select some of the repos. To be used as a regex: 'REPO[0-9]+|LIB-(a|b)'
-LISTONLY=$1; shift       # Set to anything as a 3rd parameter to list the repos only. "" '<filter>' --list
+LIST_ONLY=$1; shift       # Set to anything as a 3rd parameter to list the repos only. "" '<filter>' --list
 
-if [ -n "$LISTONLY" ];then
+if [ -n "$LIST_ONLY" ];then
    gh repo list "$OWNER" --no-archived --json name,id --limit $LIMIT |jq -r '.[].name | select( .? | match ("'"${FILTER}"'"))';# Change this gh query if so desired
    exit 0;
 fi
 
 IFS=',' read -ra BRANCH_PATTERNS <<< "$BRANCH_PATTERN_LIST"
 for BRANCH_PATTERN in "${BRANCH_PATTERNS[@]}"; do
-  while read -r REPO; do
-    echo ""
-    echo "Working with ${REPO}..."
-    REPO_ID=$(echo "$REPO" | jq -r .id)
-    REPO_NAME=$(echo "$REPO" | jq -r .name)
-    # Get the existing rule id, if any
-    RULE_ID=$(gh api graphql -f query='
-    query ($owner: String!, $repo: String!) {
-      repository(name: $repo, owner: $owner) {
-        branchProtectionRules (first: 100) {
-          edges {
-            node {
-              id
-              pattern
-            }
-          }
+while read -r REPO; do
+  echo ""
+  echo "Working with ${REPO}..."
+  REPO_ID=$(echo "$REPO" | jq -r .node_id)     # Use .node_id for repo ID
+  REPO_NAME=$(echo "$REPO" | jq -r .name)
+
+  # Get the existing rule id, if any
+  RULE_ID=$(gh api graphql -f query="
+  query ($owner: String!, $repo: String!) {
+    repository(owner: $owner, name: $repo) {
+      branchProtectionRules(first: 100) {
+        nodes { # Access the nodes array directly
+          id
+          pattern
         }
       }
-    }' -f owner="$OWNER" -f repo="$REPO_NAME" | jq '.data.repository.branchProtectionRules.edges.[] | select( .node.pattern == "'"${BRANCH_PATTERN}"'") | .node.id ');
+    }
+  }" -f owner="$OWNER" -f repo="$REPO_NAME" | jq -r '.data.repository.branchProtectionRules.nodes[] | select( .pattern == "'"$BRANCH_PATTERN"'" ) | .id') # Use .nodes instead of .edges
 
-    echo "RULE_ID: ${RULE_ID}";
-    if [[ "$RULE_ID" == "null" ]]; then
-      # If a rule does not exist, create one. Make sure to set the rules you want from https://docs.github.com/en/graphql/reference/objects#branchprotectionrule
-      gh api graphql -f query='
-        mutation($repositoryId:ID!, $branchPattern:String!) {
-          createBranchProtectionRule(input: {
-            repositoryId: $repositoryId
-            pattern: $branchPattern
+  if [[ -z "$RULE_ID" ]]; then
+    echo "No existing rule found for pattern '$BRANCH_PATTERN'. Creating a new one..."
+
+    # Create a new rule since none exists
+    gh api graphql -f query="
+      mutation($repositoryId:ID!, $branchPattern:String!) {
+        createBranchProtectionRule(input: {
+          repositoryId: $repositoryId
+          pattern: $branchPattern
             allowsForcePushes: false
-            dismissesStaleReviews: true
+            dismissesStaleReviews: false
             isAdminEnforced: false
             requireLastPushApproval: true
             requiresApprovingReviews: true
@@ -61,16 +61,23 @@ for BRANCH_PATTERN in "${BRANCH_PATTERNS[@]}"; do
             requiresConversationResolution: true
             requiresLinearHistory: true
             requiresStatusChecks: true
-          }) { clientMutationId }
-        }' -f repositoryId="$REPO_ID" -f branchPattern="$BRANCH_PATTERN";
-    else
-      # If a rule already exists, update it. Make sure to set the rules you want from https://docs.github.com/en/graphql/reference/objects#branchprotectionrule
-      gh api graphql -f query='
-        mutation($ruleId:ID!) {
-          updateBranchProtectionRule(input: {
-            branchProtectionRuleId: $ruleId
+        }) {
+          branchProtectionRule {
+            id # Retrieve the ID of the created rule
+          }
+        }
+      }" -f repositoryId="$REPO_ID" -f branchPattern="$BRANCH_PATTERN"
+
+  else
+    echo "Updating existing rule with ID '$RULE_ID'..."
+
+    # Update the existing rule
+    gh api graphql -f query="
+      mutation($branchProtectionRuleId:ID!) {
+        updateBranchProtectionRule(input: {
+          branchProtectionRuleId: $branchProtectionRuleId
             allowsForcePushes: false
-            dismissesStaleReviews: true
+            dismissesStaleReviews: false
             isAdminEnforced: false
             requireLastPushApproval: true
             requiresApprovingReviews: true
@@ -78,8 +85,13 @@ for BRANCH_PATTERN in "${BRANCH_PATTERNS[@]}"; do
             requiresConversationResolution: true
             requiresLinearHistory: true
             requiresStatusChecks: true
-          }) { clientMutationId }
-        }' -f ruleId="$RULE_ID";
-    fi
-  done <<< "$(gh repo list "$OWNER" --no-archived --json name,id --limit $LIMIT |jq -c '.[] | select( .name? | match ("'"${FILTER}"'"))')"; # Change this gh query if so desired
+        }) {
+          branchProtectionRule {
+            id
+          }
+        }
+      }" -f branchProtectionRuleId="$RULE_ID"
+  fi
+
+done <<< "$(gh repo list "$OWNER" --no-archived --json name,node_id --limit $LIMIT | jq -c '.[] | select( .name? | match ("'"${FILTER}"'"))')" # Use .node_id instead of .id
 done
